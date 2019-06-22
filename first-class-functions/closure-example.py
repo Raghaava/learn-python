36e2839d0be4ca5fc43823a9ca0a2269e74ab4a7
Raghaava <javvaji.raghava@gmail.com>
2019-06-22 16:40:12 -0700

    added closure def

--- first-class-functions/closure-example.py
+++ first-class-functions/closure-example.py
@@ -1,6 +1,11 @@
 import logging
 logging.basicConfig(filename='example.closure', level=logging.INFO)
 
+# a closure is a record storing a function[a] together with an environment.
+# The environment is a mapping associating each free variable of the function
+# (variables that are used locally, but defined in an enclosing scope) with the value or reference to which the name was bound when the closure was created.
+# Unlike a plain function, a closure allows the function to access those captured variables through the closure's copies of their values or references, even when the function is invoked outside their scope
+
 
 def closure_example(func):
     def execute(*args):
